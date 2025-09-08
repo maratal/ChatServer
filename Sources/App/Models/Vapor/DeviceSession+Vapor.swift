170179856d6ece5a29eaaf69c6428a867911fc33
Marat Al <m.alekperov@gmail.com>
2025-09-09 00:57:14 +0200

    Add url authentication for web socket (used in web browser).

--- Sources/App/Models/Vapor/DeviceSession+Vapor.swift
+++ Sources/App/Models/Vapor/DeviceSession+Vapor.swift
@@ -16,3 +16,35 @@ extension DeviceSession: ModelTokenAuthenticatable {
         true
     }
 }
+
+extension DeviceSession {
+    struct URLAccessTokenAuthenticator: RequestAuthenticator {
+        func authenticate(request: Request) -> EventLoopFuture<Void> {
+            return self.authenticate(accessToken: try? request.urlAccessToken(), for: request)
+        }
+        
+        func authenticate(accessToken: String?, for request: Request) -> EventLoopFuture<Void> {
+            do {
+                return try DeviceSession.query(on: request.db)
+                    .filter(\.$id == request.objectUUID())
+                    .first()
+                    .flatMap
+                { session -> EventLoopFuture<Void> in
+                    guard let session, accessToken?.lowercased() == session.accessToken.lowercased() else {
+                        return request.eventLoop.makeSucceededFuture(())
+                    }
+                    request.auth.login(session)
+                    return session.$user.get(on: request.db).map {
+                        request.auth.login($0)
+                    }
+                }
+            } catch {
+                return request.eventLoop.makeSucceededFuture(())
+            }
+        }
+    }
+    
+    static func urlAuthenticator() -> any Authenticator {
+        URLAccessTokenAuthenticator()
+    }
+}
